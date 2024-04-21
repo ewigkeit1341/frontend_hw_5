b15d1acbf9c76139e6915c6ad2db2b53c090c131
ewigkeit1341 <166927562+ewigkeit1341@users.noreply.github.com>
2024-04-21 15:44:08 +0200

    Add files via upload

---
+++ script/script.js
@@ -0,0 +1,35 @@
+//task1
+// let number = prompt('Enter a number')
+// let final_number = number / 100 *10
+// console.log(final_number)
+
+
+// task2
+// let num1 = prompt('Enter a number1')
+// let num2 = prompt('Enter a number2')
+// if(num1 < num2)
+// console.log(num1)
+// if(num2 < num1)
+// console.log(num2)
+
+
+// task3
+// let number = prompt('Enter a number')
+// if(number < 100)
+//     console.log('Число меньше 100')
+// else if(number > 100)
+//     console.log('Число больше 100')
+// else
+//     console.log('Число равно 100')
+
+
+// task4
+let user_name = prompt('Enter your name')
+let user_age = +prompt('Enter your age')
+if(user_age >= 18)
+console.log(`Hello, ${user_name}`)
+else
+console.log(`Hi, ${user_name}`)
+
+
+// I did it!!!!
